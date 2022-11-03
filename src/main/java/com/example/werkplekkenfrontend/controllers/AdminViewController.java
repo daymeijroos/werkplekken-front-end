c7e1808b9c71a8dfbdf7fdc32261e8c0b5e87e8a
Jeroen <n400397@student.hsleiden.nl>
2022-11-03 14:45:14 +0100

    Added: Navbar to AdminViewController. Changed: set maxwidth from admin-edit-building-view.fxml's VBox holding the text areas

--- src/main/java/com/example/werkplekkenfrontend/controllers/AdminViewController.java
+++ src/main/java/com/example/werkplekkenfrontend/controllers/AdminViewController.java
@@ -1,8 +1,13 @@
 package com.example.werkplekkenfrontend.controllers;
 
 import com.example.werkplekkenfrontend.Main;
+import com.example.werkplekkenfrontend.elements.NavBarElement;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 
 public class AdminViewController implements ViewController{
+    @FXML
+    public VBox main_container;
 
     public void onBuildingsButtonClick(){
         ViewController controller = Main.sceneController.showView("admin-buildings-view.fxml");
@@ -15,6 +20,6 @@ public class AdminViewController implements ViewController{
 
     @Override
     public void updateView() {
-
+        main_container.getChildren().add(new NavBarElement().getBuildingBox());
     }
 }
