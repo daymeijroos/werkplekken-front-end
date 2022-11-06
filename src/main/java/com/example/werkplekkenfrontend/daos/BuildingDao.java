20922c2476d41771f6db01202d72584ec445cebe
Jeroen <s1137642@student.hsleiden.nl>
2022-11-06 21:22:22 +0100

    Updated: Cleaned up FloorDao

--- src/main/java/com/example/werkplekkenfrontend/daos/BuildingDao.java
+++ src/main/java/com/example/werkplekkenfrontend/daos/BuildingDao.java
@@ -1,24 +1,9 @@
 package com.example.werkplekkenfrontend.daos;
 
 import com.example.werkplekkenfrontend.models.Building;
-import com.example.werkplekkenfrontend.models.Reservation;
-import com.example.werkplekkenfrontend.models.User;
-import com.example.werkplekkenfrontend.project_settings;
 import com.example.werkplekkenfrontend.services.HttpService;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.UUID;
 
--- src/main/java/com/example/werkplekkenfrontend/daos/FloorDao.java
+++ src/main/java/com/example/werkplekkenfrontend/daos/FloorDao.java
@@ -1,96 +1,79 @@
 package com.example.werkplekkenfrontend.daos;
 
 import com.example.werkplekkenfrontend.models.Floor;
-import com.example.werkplekkenfrontend.models.Floor;
-import com.example.werkplekkenfrontend.project_settings;
 
 import com.example.werkplekkenfrontend.services.HttpService;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.OutputStream;
-import java.net.*;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class FloorDao implements Dao<Floor> {
+    private final HttpService httpService;
+    private final ObjectMapper objectMapper;
+
+    public FloorDao(HttpService httpService, ObjectMapper objectMapper){
+        this.httpService = httpService;
+        this.objectMapper = objectMapper;
+    }
+
     @Override
     public ArrayList<Floor> getAll() {
-        HttpService http = new HttpService();
-        String url = project_settings.baseURL + "/api/floor";
-        String response = http.getWithURL(url);
+        String url = "/api/floor";
+        String response = httpService.getWithURL(url);
         ObjectMapper mapper = new ObjectMapper();
-        ArrayList<Floor> floors = null;
-
         try {
-            floors = mapper.readValue(response, new TypeReference<ArrayList<Floor>>(){});
+            ArrayList<Floor> floors = mapper.readValue(response, new TypeReference<>() {
+            });
+            return floors;
         } catch (Exception e) {
             e.printStackTrace();
-        };
-
-        return floors;
+            return null;
+        }
     }
 
     @Override
     public Floor get(UUID id) {
-        HttpService http = new HttpService();
-        String url = project_settings.baseURL + "/api/floor/" + id;
-        String response = http.getWithURL(url);
+        String url = "/api/floor/" + id;
+        String response = httpService.getWithURL(url);
         ObjectMapper mapper = new ObjectMapper();
         Floor floor = null;
-
         try {
             floor = mapper.readValue(response, Floor.class);
         } catch (Exception e) {
             e.printStackTrace();
-        };
-
+        }
         return floor;
     }
 
     @Override
     public int post(Floor object) {
-        HttpService http = new HttpService();
-        String url = project_settings.baseURL + "/api/floor";
+        String url = "/api/floor";
         ObjectMapper mapper = new ObjectMapper();
-        String json = null;
-
         try {
-            json = mapper.writeValueAsString(object);
+            String json = mapper.writeValueAsString(object);
+            return httpService.postWithURLandJSONreturnsCode(url, json);
         } catch (Exception e) {
             e.printStackTrace();
             return 400;
         }
-
-        return http.postWithURLandJSONreturnsCode(url, json);
     }
 
     @Override
     public int patch(Floor object) {
-        HttpService http = new HttpService();
-        String url = project_settings.baseURL + "/api/floor/" + object.getId();
+        String url = "/api/floor/" + object.getId();
         ObjectMapper mapper = new ObjectMapper();
-        String json = null;
-
         try {
-            json = mapper.writeValueAsString(object);
+            String json = mapper.writeValueAsString(object);
+            return httpService.patchWithURL(url, json);
         } catch (Exception e) {
             e.printStackTrace();
             return 400;
         }
-
-        return http.patchWithURL(url, json);
     }
 
     @Override
     public int delete(Floor object) {
-        HttpService http = new HttpService();
-        String url = project_settings.baseURL + "/api/floor/" + object.getId();
-        return http.deleteWithURL(url);
+        String url = "/api/floor/" + object.getId();
+        return httpService.deleteWithURL(url);
     }
 }
