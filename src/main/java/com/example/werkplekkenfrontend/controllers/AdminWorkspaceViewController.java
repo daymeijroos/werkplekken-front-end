3b9704138130f8d5249578716dfb5d9cd3f3fabd
Hassan <s1136140@student.hsleiden.nl>
2022-11-07 01:32:16 +0100

    fix: not working patch function commented out

--- src/main/java/com/example/werkplekkenfrontend/controllers/AdminWorkspaceViewController.java
+++ src/main/java/com/example/werkplekkenfrontend/controllers/AdminWorkspaceViewController.java
@@ -1,25 +1,19 @@
 package com.example.werkplekkenfrontend.controllers;
 
 import com.example.werkplekkenfrontend.Main;
-import com.example.werkplekkenfrontend.daos.LoginDao;
 import com.example.werkplekkenfrontend.daos.SpaceDao;
 import com.example.werkplekkenfrontend.elements.AdminWorkspaceElement;
 import com.example.werkplekkenfrontend.elements.NavBarElement;
-import com.example.werkplekkenfrontend.models.Facility;
 import com.example.werkplekkenfrontend.models.Space;
 import com.example.werkplekkenfrontend.services.HttpService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
-import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.UUID;
 
-public class AdminWorkspaceViewController implements ViewController{
+public class AdminWorkspaceViewController implements ViewController {
 
     private SpaceDao spaceDao = new SpaceDao(new HttpService(), new ObjectMapper());
     @FXML
@@ -34,14 +28,15 @@ public class AdminWorkspaceViewController implements ViewController{
         controller.updateView();
     }
 
-    private void showSpacesOnView(){
+    private void showSpacesOnView() {
         List<Space> spacesFromDao = spaceDao.getAll();
-        for(Space space : spacesFromDao){
-            AdminWorkspaceElement element = new AdminWorkspaceElement(this,space);
+        for (Space space : spacesFromDao) {
+            AdminWorkspaceElement element = new AdminWorkspaceElement(this, space);
             workspaces_container.getChildren().add(element.getWorkspaceBox());
         }
     }
 
+    /*
     @FXML
     public void onEditWorkspaceButtonClick(Space space) {
         AdminEditWorkspaceViewController controller = (AdminEditWorkspaceViewController) Main.sceneController.showView("space-edit-workspace.fxml");
@@ -49,6 +44,8 @@ public class AdminWorkspaceViewController implements ViewController{
         controller.updateView();
 
     }
+
+     */
 
 
     @Override
@@ -61,14 +58,14 @@ public class AdminWorkspaceViewController implements ViewController{
 
     @FXML
     void onApplyClick(ActionEvent event) {
-        ViewController controller = Main.sceneController.showView("admin-space-view.fxml");
+        ViewController controller = Main.sceneController.showView("admin-space-view-v2.fxml");
         controller.updateView();
 
     }
 
     @FXML
     void onCancelClick(ActionEvent event) {
-        ViewController controller = Main.sceneController.showView("admin-space-view.fxml");
+        ViewController controller = Main.sceneController.showView("admin-space-view-v2.fxml");
         controller.updateView();
 
     }
