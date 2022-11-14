94a9ca076d8831294e9f18ba05b932b96ab91d7b
daymeijroos <pekieend@gmail.com>
2022-11-14 16:57:01 +0100

    Feature: added default.fxml as default page, new pages are rendered into this file. Allows for standard elements like navbar and popup.

--- src/main/java/com/example/werkplekkenfrontend/controllers/SceneController.java
+++ src/main/java/com/example/werkplekkenfrontend/controllers/SceneController.java
@@ -1,7 +1,9 @@
 package com.example.werkplekkenfrontend.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
@@ -10,13 +12,19 @@ public class SceneController {
     private Stage stage;
     Scene scene;
 
+    @FXML
+    Pane page;
+
+    @FXML
+    Pane navBar;
+
     public ViewController showView(String xmlFileName) {
         ViewController controller = null;
 
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/werkplekkenfrontend/" + xmlFileName));
             VBox root = loader.load();
-            scene.setRoot(root);
+            page.getChildren().setAll(root);
             controller = loader.getController();
             stage.show();
         } catch(Exception e) {
@@ -27,8 +35,15 @@ public class SceneController {
 
     public void init(Stage stage) {
         this.stage = stage;
-        scene = new Scene(new VBox());
-        stage.setScene(scene);
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/werkplekkenfrontend/default.fxml"));
+            loader.setController(this);
+            VBox root = loader.load();
+            scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void showPopup(Popup popup) {
