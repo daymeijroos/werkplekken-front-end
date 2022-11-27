b2792e5dedd8991191847f88e94ef21878d33c3c
Dylan Smit <s1135852@student.hsleiden.nl>
2022-11-27 17:15:24 +0100

    !change: added lombok dependency which allowed me to remove a whole chunk of boilerplate code

--- src/main/java/com/example/werkplekkenfrontend/models/Reservation.java
+++ src/main/java/com/example/werkplekkenfrontend/models/Reservation.java
@@ -1,17 +1,15 @@
 package com.example.werkplekkenfrontend.models;
 
-public class Reservation {
-    public Reservation() {}
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-    public Reservation(String id, String userId, String dateIn, String dateOut, int amountOfPeople, String spaceId, String state) {
-        this.id = id;
-        this.userId = userId;
-        this.dateIn = dateIn;
-        this.dateOut = dateOut;
-        this.amountOfPeople = amountOfPeople;
-        this.spaceId = spaceId;
-        this.state = state;
-    }
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reservation {
 
     public String id;
     public String userId;
@@ -20,4 +18,5 @@ public class Reservation {
     public int amountOfPeople;
     public String spaceId;
     public String state;
+
 }
--- src/main/java/com/example/werkplekkenfrontend/models/User.java
+++ src/main/java/com/example/werkplekkenfrontend/models/User.java
@@ -1,19 +1,20 @@
 package com.example.werkplekkenfrontend.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
     public String id;
     public String name;
     public String lastName;
     public String email;
-
-    public User() {}
-
-    public User(String id, String name, String lastName, String email) {
-        this.id = id;
-        this.name = name;
-        this.lastName = lastName;
-        this.email = email;
-    }
 
     public String getId() {
         return id;
